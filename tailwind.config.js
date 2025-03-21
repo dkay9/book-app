282fa2a7ff5147277290b593de3d204fd0e803dc
Kalu David <kaludavid411@gmail.com>
2025-03-21 08:30:45 +0100

    new push

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./views/**/*.ejs"], // Scan all EJS files for Tailwind classes
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  };
+  
